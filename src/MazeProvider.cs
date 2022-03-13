a4717373910154598fdfaf8690fa065844561bbc
xieheng <xieheng84@163.com>
2022-03-13 23:20:41 +0800

    增加点注释

--- src/MazeProvider.cs
+++ src/MazeProvider.cs
@@ -2,12 +2,14 @@ namespace SimpleX.Maze
 {
     public static class MazeProvider
     {
+        // 创建矩形迷宫
         public static RectangleMazeField CreateRectangleMaze(int width, int height)
         {
             RectangleMaze maze = new RectangleMaze();
             return maze.Create(width, height);
         }
 
+        // 创建有房间的矩形迷宫，即地牢
         public static RectangleMazeField CreateRectangleWithRoomMaze(int width, int height, int minRoomWidth, int maxRoomWidth, int minRoomHeight, int maxRoomHeight, int maxRoomCount)
         {
             RectangleWithRoomMaze maze = new RectangleWithRoomMaze();
