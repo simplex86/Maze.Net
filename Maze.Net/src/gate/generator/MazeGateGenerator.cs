e5da7d0b387991fcacd0784040bb365da2ac767a
xieheng <xieheng84@163.com>
2026-05-31 23:16:40 +0800

    删除废弃代码

--- Maze.Net/src/gate/generator/MazeGateGenerator.cs
+++ Maze.Net/src/gate/generator/MazeGateGenerator.cs
@@ -1,24 +1,8 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SimplexLab.Maze
 {
-    /// <summary>
-    /// 出入口位置
-    /// </summary>
-    public enum EGatePosition
-    {
-        /// <summary>
-        /// 任意位置
-        /// </summary>
-        Any = 1,
-        /// <summary>
-        /// 边缘上
-        /// </summary>
-        Edge = 2,
-    }
-
     /// <summary>
     /// 迷宫出入口
     /// </summary>
@@ -59,41 +43,6 @@ namespace SimplexLab.Maze
         {
             var gate = await Task.Run(() => Generate(field));
             return gate;
-        }
-
-        /// <summary>
-        /// 查找所有边缘顶点（至少有一条边界边的顶点）
-        /// </summary>
-        private static List<int> FindEdgeVertices(MazeField field)
-        {
-            var list = new List<int>();
-            for (int v = 0; v < field.VertexCount; v++)
-            {
-                foreach (var edge in field.Graph[v])
-                {
-                    if (edge.Neighbor == -1)
-                    {
-                        list.Add(v);
-                        break;
-                    }
-                }
-            }
-            return list;
-        }
-
-        /// <summary>
-        /// 根据位置选项获取候选顶点列表
-        /// </summary>
-        private static List<int> GetCandidates(MazeField field, List<int> edgeVertices, EGatePosition position)
-        {
-            if (position == EGatePosition.Edge)
-                return new List<int>(edgeVertices);
-
-            var list = new List<int>(field.VertexCount);
-            for (int i = 0; i < field.VertexCount; i++)
-                list.Add(i);
-
-            return list;
         }
     }
 }
