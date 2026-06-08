3d2ad874f99bfacb286263e57bc71a4241880d80
xieheng <xieheng84@163.com>
2026-06-08 17:13:01 +0800

    修改三角形迷宫出入口位置

--- Maze.Net-Application/MainForm.Designer.cs
+++ Maze.Net-Application/MainForm.Designer.cs
@@ -255,7 +255,7 @@ namespace Maze.TApplication
             MinimumSize = new Size(1080, 780);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Maze Generator v1.2.115";
+            Text = "Maze Generator v1.2.119";
             splitContainer1.Panel1.ResumeLayout(false);
             splitContainer1.Panel1.PerformLayout();
             splitContainer1.Panel2.ResumeLayout(false);
